ea63b320ff3ae2cbacb67791b576d161f5abc14f
hezebin <86744316@qq.com>
2024-04-12 00:51:00 +0800

    feature: update README.md

--- cron/cron.go
+++ cron/cron.go
@@ -12,7 +12,7 @@ func Run(ctx context.Context) error {
 	c := cron.New(cron.WithSeconds())
 
 	// 添加一个任务，每10分钟执行一次
-	_, err := c.AddFunc("* */10 * * * *", func() {
+	_, err := c.AddFunc("0 */10 * * * *", func() {
 		fmt.Println("Cron job executed!", time.Now().Format(time.DateTime))
 	})
 	if err != nil {
